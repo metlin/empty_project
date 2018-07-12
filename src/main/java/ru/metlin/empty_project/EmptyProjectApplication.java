75921f0437147d3c443a4be4097feacf5d040204
Mikhail Metlin <mihailmetlin@mail.ru>
2018-07-12 22:05:10 +0300

    Развернул пустой проект

---
+++ src/main/java/ru/metlin/empty_project/EmptyProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.metlin.empty_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyProjectApplication.class, args);
+	}
+}
